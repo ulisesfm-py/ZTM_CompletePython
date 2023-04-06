91904704ba82c0bd3b4543bb87d2adbcaf493c81
ulisesfm-py <ulisesfm.py@gmail.com>
2023-04-06 12:10:42 +0200

    added html message and integrated it into the email_sender

--- 13_Scripting/email-playground/email_sender.py
+++ 13_Scripting/email-playground/email_sender.py
@@ -1,2 +1,22 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path  # os.path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Ulises Fernández'
+email['to'] = 'ulisestgy@gmail.com'
+email['subject'] = 'this is a Python exercise'
+
+#email.set_content('I am practicing email sending through Python')
+# email.set_content(html.substitute({'name' = 'TinTin'}), 'html')
+email.set_content(html.substitute({'name': 'Tintin'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    # this will be confusing, but you just need to copy
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('ulisestgy@gmail.com', 'notarealpassword')
+    smtp.send_message(email)
+    print('all good')
